3047884931b39e672b3190a80e4da42d99069ee6
jenn <epistemicpolymathic5@gmail.com>
2017-01-18 20:50:59 -0500

    interact.js for the 'Help-Box' text working on it...It works, but I cannot set default.

--- interact.js
+++ interact.js
@@ -1,61 +1,39 @@
-
-$(document).getElementById("nameInput").onfocus( function () {
-    document.getElementById("help-text").value = "Enter your name as you want it to appear on the bill.";
-    document.getElementById("help-text").innerHTML = "Enter your name as you want it to appear on the bill.";
-    $("textarea#help-text").text("Enter your name as you want it to appear on the bill.");
-    $('#help-text').text("Enter your name as you want it to appear on the bill.");
-});
-
-
-
+//Help-Text Interactions
 // When the document loads this function plays out.
 $(document).ready(function () {
 
+    $("#nameInput").focus(function () {
 
-   $(document).getElementById("nameInput").focus( function () {
-        document.getElementById("help-text").value = "Enter your name as you want it to appear on the bill.";
-        document.getElementById("help-text").innerHTML = "Enter your name as you want it to appear on the bill.";
-        $("textarea#help-text").text("Enter your name as you want it to appear on the bill.");
-        $('#help-text').text("Enter your name as you want it to appear on the bill.");
+        $("#helpText").val("Enter your name as you want it to appear on the bill.");
+
     });
 
-    $('help-text-field').focus(function () {
+    $("#emailInput").focus(function () {
+
+        $("#helpText").val("Enter your email address in the format user@domain");
+    });
 
 
-            if (id == "nameInput") {
+    $("#address").focus(function () {
 
-                document.getElementById("help-text").value = "Enter your name as you want it to appear on the bill.";
-                $("textarea#help-text").text("Enter your name as you want it to appear on the bill.");
-                $('#help-text').text("Enter your name as you want it to appear on the bill.");
-            }
+        $("#helpText").val("Enter Address for shipping");
 
+    });
 
-            if (id == "emailInput") {
+    $("#submitButton").focus(function () {
 
+        $("#helpText").val("Submit Order");
 
-                document.getElementById("help-text").innerHTML = "Enter your email address in the format user@domain";
-            }
+    });
 
 
-            if (id == "submitButton") {
+    $("#resetButton").focus(function () {
 
+        $("#helpText").val("Clear All");
 
-                document.getElementById("help-text").innerHTML = "Enter Address for shipping";
-            }
+    });
 
+    $("#helpText").val("This is to provide context-sensitive help. " +
+    "Click on any input field or use the TAB key to get more information about the input field.");
 
-            if (id == "resetButton") {
-
-                document.getElementById("help-text").innerHTML = "Submit Order";
-
-            }
-
-            if ((id == " ") || (id == "") || (id == null)) {
-
-                document.getElementById("help-text").innerHTML = "This is to provide context-sensitive help. " +
-                    "Click on any input field or use the TAB key to get more information about the input field."
-
-
-            }
-        });
 });
